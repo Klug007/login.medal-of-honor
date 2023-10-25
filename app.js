bcafd6839058b42993456ca4cd2c9b48849c8c88
Klug007 <tilla.yosinbek@gmail.com>
2023-10-25 21:38:59 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const form = document.querySelector("form");
+const Email = document.querySelector("#email");
+const Password = document.querySelector("#password");
+const show = document.querySelector("#show");
+const natija = document.querySelector(".natija");
+
+form.addEventListener("submit", formSubmit);
+console.log(window);
+function formSubmit(e) {
+    e.preventDefault();
+    let kirishemail = "ali@gmail.com";
+    let kirishparoli = "123456";
+    if (kirishemail === Email.value && kirishparoli === Password.value) {
+        natija.innerHTML = "Saytga hush kelibsiz";
+        setTimeout(() => {
+            window.location.pathname = "/home";
+            localStorage.setItem('key', true)
+        }, 3000);
+    } else {
+        natija.innerHTML = "parol yoki login noto'g'ri";
+
+        setTimeout(() => {
+            natija.innerHTML = "";
+        }, 5000);
+    }
+    Email.value = "";
+    Password.value = "";
+}
+
+show.addEventListener("change", function () {
+    let checkbox = show.checked;
+    if (checkbox) {
+        Password.type = "text";
+    } else {
+        Password.type = "password";
+    }
+});
+
+
---
+++ home/app.js
@@ -0,0 +1,15 @@
+function checklogin() {
+    let localstorage = localStorage.getItem('key')
+    if(localstorage) {
+        console.log('hammasi joyida');
+    } else {
+        window.location.pathname = '/'
+    }
+}
+
+checklogin()
+
+function logout() {
+    localStorage.setItem('key', '')
+    window.location.pathname = '/'
+}
